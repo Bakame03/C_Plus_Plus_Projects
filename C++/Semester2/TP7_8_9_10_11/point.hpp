f4f08c4ea128bf935acf71fdce80576f7ac2bbd1
bakame03 <nganjialdoalex@gmail.com>
2026-03-18 11:29:50 +0100

    feat: add Point class definition with coordinates, constructors, accessors, and `isOver` method.

---
+++ C++/Semester2/TP7_8_9_10_11/point.hpp
@@ -0,0 +1,26 @@
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include <iostream>
+using namespace std;
+
+typedef unsigned int uint;
+
+class Point {
+    private:
+        static const uint size = 5;
+        uint x, y;
+    public:
+        Point(uint x, uint y);
+        Point(const Point& p);
+        Point(istream& is);
+        ~Point();
+        inline uint getX() const { return x; }
+        inline uint getY() const { return y; }
+        inline void setXY(uint _x, uint _y) { x = _x; y = _y; }
+        bool isOver(uint mouseX, uint mouseY) const;
+        
+    
+};
+
+#endif
