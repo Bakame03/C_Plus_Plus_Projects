d5dcd37c9eb355955cc4558d36cc308e53a0c50e
bakame03 <nganjialdoalex@gmail.com>
2026-03-23 09:21:31 +0100

    feat: Implement basic image viewer with zoom functionality via menus and a dockable toolbar.

--- R2_02_IHM/TP4/tp4/mainwindow.cpp
+++ R2_02_IHM/TP4/tp4/mainwindow.cpp
@@ -1,7 +1,110 @@
 #include "mainwindow.h"
+#include <QApplication>
+#include <QFileDialog>
+#include <QDockWidget>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QFileDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-{}
+{
+    scrollArea.setWidget(&imageLabel);
+    setCentralWidget(&scrollArea);
+
+    // === Menu Fichier ===
+    QMenu *fichierMenu = new QMenu(QString::fromUtf8("&Fichier"), this);
+    menuBar()->addMenu(fichierMenu);
+
+    actionOuvrir = new QAction(QString::fromUtf8("Ouvrir image"), this);
+    actionOuvrir->setShortcut(QKeySequence::Open);
+    connect(actionOuvrir, SIGNAL(triggered()), this, SLOT(ouvrirImage()));
+    fichierMenu->addAction(actionOuvrir);
+
+    fichierMenu->addSeparator();
+
+    actionQuitter = new QAction(QString::fromUtf8("Quitter"), this);
+    actionQuitter->setShortcut(QKeySequence::Quit);
+    connect(actionQuitter, SIGNAL(triggered()), qApp, SLOT(quit()));
+    fichierMenu->addAction(actionQuitter);
+
+    // === Menu Affichage ===
+    QMenu *affichageMenu = new QMenu(QString::fromUtf8("&Affichage"), this);
+    menuBar()->addMenu(affichageMenu);
+
+    actionZoomMoins = new QAction(QString::fromUtf8("Zoom -"), this);
+    connect(actionZoomMoins, SIGNAL(triggered()), this, SLOT(zoomMoins()));
+    affichageMenu->addAction(actionZoomMoins);
+
+    actionZoomPlus = new QAction(QString::fromUtf8("Zoom +"), this);
+    connect(actionZoomPlus, SIGNAL(triggered()), this, SLOT(zoomPlus()));
+    affichageMenu->addAction(actionZoomPlus);
+
+    actionTailleNormale = new QAction(QString::fromUtf8("Taille normale"), this);
+    connect(actionTailleNormale, SIGNAL(triggered()), this, SLOT(tailleNormale()));
+    affichageMenu->addAction(actionTailleNormale);
+
+    // === DockWidget pour les outils ===
+    QDockWidget *dock = new QDockWidget(QString::fromUtf8("Outils"), this);
+    QWidget *dockWidget = new QWidget(dock);
+    QVBoxLayout *dockLayout = new QVBoxLayout(dockWidget);
+
+    QPushButton *btnZoomMoins = new QPushButton(QString::fromUtf8("Zoom -"));
+    QPushButton *btnZoomPlus = new QPushButton(QString::fromUtf8("Zoom +"));
+    QPushButton *btnTailleNormale = new QPushButton(QString::fromUtf8("Taille normale"));
+
+    dockLayout->addWidget(btnZoomMoins);
+    dockLayout->addWidget(btnZoomPlus);
+    dockLayout->addWidget(btnTailleNormale);
+    dockLayout->addStretch(); // Pousse les boutons vers le haut
+
+    dockWidget->setLayout(dockLayout);
+    dock->setWidget(dockWidget);
+    addDockWidget(Qt::RightDockWidgetArea, dock);
+
+    // Connexion des boutons aux mêmes slots
+    connect(btnZoomMoins, SIGNAL(clicked()), this, SLOT(zoomMoins()));
+    connect(btnZoomPlus, SIGNAL(clicked()), this, SLOT(zoomPlus()));
+    connect(btnTailleNormale, SIGNAL(clicked()), this, SLOT(tailleNormale()));
+}
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::ouvrirImage()
+{
+    QString fileName = QFileDialog::getOpenFileName(
+        this,
+        QString::fromUtf8("Ouvrir une image"),
+        "",
+        QString::fromUtf8("Images (*.png *.jpg *.jpeg)"));
+
+    if (fileName != "") {
+        image.load(fileName);
+        imageLabel.setPixmap(QPixmap::fromImage(image));
+        imageLabel.setScaledContents(true);
+        imageLabel.resize(image.size());
+        setWindowTitle(fileName);
+    }
+}
+void MainWindow::zoomPlus()
+{
+    zoom *= 1.25;
+    changeZoom();
+}
+
+void MainWindow::zoomMoins()
+{
+    zoom *= 0.8;
+    changeZoom();
+}
+
+void MainWindow::tailleNormale()
+{
+    zoom = 1.0;
+    changeZoom();
+}
+
+void MainWindow::changeZoom()
+{
+    imageLabel.resize(zoom * image.size());
+}
--- R2_02_IHM/TP4/tp4/mainwindow.h
+++ R2_02_IHM/TP4/tp4/mainwindow.h
@@ -2,7 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QLabel>
+#include <QScrollArea>
+#include <QImage>
+#include <QMenuBar>
+#include <QMenu>
+#include <QAction>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,5 +15,25 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void ouvrirImage();
+    void zoomPlus();
+    void zoomMoins();
+    void tailleNormale();
+
+private:
+    QLabel imageLabel;
+    QScrollArea scrollArea;
+    QImage image;
+
+    double zoom = 1.0;
+    void changeZoom();
+
+    QAction *actionOuvrir;
+    QAction *actionQuitter;
+    QAction *actionZoomPlus;
+    QAction *actionZoomMoins;
+    QAction *actionTailleNormale;
 };
 #endif // MAINWINDOW_H
