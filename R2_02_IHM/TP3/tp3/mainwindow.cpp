08ebb1e8f79850022c1a63db879a1e340996e400
bakame03 <nganjialdoalex@gmail.com>
2026-03-23 08:34:38 +0100

    feat: Implement initial clock drawing with `paintEvent`, helper functions, and basic window styling.

--- R2_02_IHM/TP3/tp3/mainwindow.cpp
+++ R2_02_IHM/TP3/tp3/mainwindow.cpp
@@ -4,6 +4,38 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
     setWindowTitle(QString::fromUtf8("Horloge"));
+    resize(300, 300);
+    setStyleSheet("QMainWindow {background: rgb(150,200,250);}");
 }
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    
+    // On fait ici les affichages graphiques :
+    afficheFond(painter);
+    afficheGraduations(painter);
+    afficheChiffres(painter);
+    afficheAiguilles(painter);
+}
+
+void MainWindow::afficheFond(QPainter &painter)
+{
+    painter.setPen(QPen(Qt::black, 2));
+    painter.drawEllipse(10, 10, 280, 280);
+}
+
+void MainWindow::afficheGraduations(QPainter &painter)
+{
+}
+
+void MainWindow::afficheChiffres(QPainter &painter)
+{
+}
+
+void MainWindow::afficheAiguilles(QPainter &painter)
+{
+}
--- R2_02_IHM/TP3/tp3/mainwindow.h
+++ R2_02_IHM/TP3/tp3/mainwindow.h
@@ -2,7 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QPaintEvent>
+#include <QPainter>
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,5 +11,14 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
+private:
+    void afficheFond(QPainter &painter);
+    void afficheGraduations(QPainter &painter);
+    void afficheChiffres(QPainter &painter);
+    void afficheAiguilles(QPainter &painter);
 };
 #endif // MAINWINDOW_H
