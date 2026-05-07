f5cf29cb5b83fd8c571250ae807f2e09c91bd60e
bakame03 <nganjialdoalex@gmail.com>
2026-05-07 16:47:18 +0200

    feat: add QNetworkAccessManager support to fetch remote JSON data

--- R2_02_IHM/TP7/tp7/mainwindow.cpp
+++ R2_02_IHM/TP7/tp7/mainwindow.cpp
@@ -1,7 +1,23 @@
 #include "mainwindow.h"
+#include <QNetworkRequest>
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {}
 
 MainWindow::~MainWindow() {}
+
+void MainWindow::chargeJSON(QString url)
+{
+    QNetworkRequest request = QNetworkRequest(QUrl(url));
+    QNetworkReply* reply = manager.get(request);
+    
+    // connection à un signal qui indique quand le fichier est téléchargé
+    QObject::connect(reply, &QNetworkReply::finished, [=]() {
+        // lecture des données
+        QString ReplyText = reply->readAll();
+        
+        reply->deleteLater();
+    });
+}
